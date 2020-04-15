0ea35e4f82034a738b8794d199c0937c5ce63fa3
ljm <1914660874@qq.com>
2020-04-15 09:14:39 +0800

    新建User.java,提交到本地仓库

---
+++ gittest/src/main/java/com/ljm/User.java
@@ -0,0 +1,17 @@
+package com.ljm;/**
+ * @Author LJM
+ * @Date 2020/4/15 9:08
+ * @Version 1.0
+ */
+
+/**
+ * @ClassName User
+ * @Description 测试git
+ * @Author ljm
+ * @Date 9:08 2020/4/15
+ * @Version 2.1
+ **/
+public class User {
+    private Integer id;
+    private String name;
+}
